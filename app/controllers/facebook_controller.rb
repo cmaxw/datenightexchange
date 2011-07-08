e6a55324a57943bbaac43b7e9b0f3d0911e89952
Charles Max Wood <chuck@teachmetocode.com>
2011-07-07 22:08:51 -0600

    facebook signed request handling

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,8 +1,46 @@
-require "base64"
+require 'base64'
+require 'openssl'
 
 class FacebookController < ApplicationController
   def canvas
-    @signed_request = Base64.decode64(params[:signed_request])
-    logger.info @signed_request.inspect
+    if fb_data = facebook_signed_request?
+      logger.info fb_data.inspect
+    end
   end
+
+  def facebook_signed_request? (signed_request, secret)
+
+    def base64_url_decode str
+      encoded_str = str.gsub('-','+').gsub('_','/')
+      encoded_str += '=' while !(encoded_str.size % 4).zero?
+      Base64.decode64(encoded_str)
+    end
+
+    def str_to_hex s
+      (0..(s.size-1)).to_a.map do |i|
+        number = s[i].to_s(16)
+        (s[i] < 16) ? ('0' + number) : number
+      end.join
+    end
+
+    #decode data
+    encoded_sig, payload = signed_request.split('.')
+    sig = str_to_hex(base64_url_decode(encoded_sig))
+    data = ActiveSupport::JSON.decode base64_url_decode(payload)
+
+    if data['algorithm'].to_s.upcase != 'HMAC-SHA256'
+  #    Rails.logger.error 'Unknown algorithm. Expected HMAC-SHA256'
+      return false
+    end
+
+    #check sig
+    expected_sig = OpenSSL::HMAC.hexdigest('sha256', secret, payload)
+    if expected_sig != sig
+  #    Rails.logger.error 'Bad Signed JSON signature!'
+      return false
+    end
+
+    data
+  end
+  
 end
