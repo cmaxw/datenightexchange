64ac1b801ae5efb46ba9e869f4458c1dd9aa5f2a
Charles Max Wood <chuck@teachmetocode.com>
2011-07-07 21:31:03 -0600

    clean up facebook handler

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,5 +1,8 @@
+require "base64"
+
 class FacebookController < ApplicationController
   def canvas
+    @signed_request = Base64.decode64(params[:signed_request])
+    logger.info @signed_request.inspect
   end
-
 end
